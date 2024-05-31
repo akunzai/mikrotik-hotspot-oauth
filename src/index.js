da93604fafbc568e6351109af0922ebb1652888e
Charley Wu <akunzai@gmail.com>
2024-05-31 19:03:15 +0800

    Migrate from Webpack to Vite
    - https://vitejs.dev/

--- src/index.js
+++ src/index.js
@@ -1,48 +1,49 @@
+import md5 from 'blueimp-md5';
+window.md5 = md5;
+import { createRemoteJWKSet, jwtVerify } from 'jose';
 import Keycloak from 'keycloak-js';
-import {
-  getPassword, getUserName, loadKeycloakJson, verifyToken,
-} from './loginUtils';
+
+async function verifyToken(keycloak) {
+  try {
+    const jwkSet = createRemoteJWKSet(new URL(`${keycloak.authServerUrl}/realms/${keycloak.realm}/protocol/openid-connect/certs`));
+    const { payload } = await jwtVerify(keycloak.token, jwkSet);
+    return payload;
+  } catch (e) {
+    throw new Error(`invalid token: ${e}`);
+  }
+}
 
 function setError(message) {
   const errorElement = document.getElementById('error');
   errorElement.innerHTML = `<br /><div style="color: #FF8080; font-size: 9px">${message}</div>`;
 }
 
-function clear() {
+async function main() {
+  const keycloak = new Keycloak("/keycloak.json");
+  const authenticated = await keycloak.init({
+    onLoad: 'login-required'
+  });
+  if (!authenticated) {
+    setError('failed to initialize SSO, please check configuration');
+    return;
+  }
+  const claims = await verifyToken(keycloak);
+  const username = claims[claims.n];
+  const password = claims[claims.np || 's'];
+  if (!username || !password) {
+    setError(`Client ${keycloak.clientId} is not configured. Please check client mapper.`);
+    return;
+  }
   const rootElement = document.getElementById('root');
   rootElement.innerHTML = 'Please wait...';
+  doLogin(username, password);
 }
-if (!error) {
-  loadKeycloakJson()
-    .then((json) => {
-      const keycloak = new Keycloak('/keycloak.json');
-      keycloak.init({
-        onLoad: 'login-required',
-        promiseType: 'native',
-      }).then((authenticated) => {
-        if (authenticated) {
-          verifyToken(keycloak.token, json).then((decodedToken) => {
-            const password = getPassword(decodedToken);
-            const userName = getUserName(decodedToken);
-            if (!userName || !password) {
-              setError(`Client ${json.resource} is not configured. Please check client mapper.`);
-            } else {
-              doLogin(userName, password);
-              clear();
-            }
-          }).catch((ve) => {
-            setError(`failed to verify token, please check configuration: ${ve}`);
-          });
-        } else {
-          setError('failed to initialize SSO, please check configuration');
-        }
-      }).catch((e) => {
-        setError(`failed to initialize SSO, please check configuration: ${e}`);
-      });
-    })
-    .catch((e) => {
-      setError(`failed to load keycloak.json, please check configuration: ${e}`);
-    });
-} else {
+
+if (error) {
   setError(`${error}`);
+} else {
+  main()
+    .catch((error) => {
+      setError(`failed to initialize SSO, please check configuration: ${error}`);
+    });
 }
